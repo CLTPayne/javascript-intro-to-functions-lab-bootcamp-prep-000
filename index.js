10182e64b935b6ba958da5fe78879f3d32c44431
Charlotte Payne <cltpayne@gmail.com>
2018-01-13 14:49:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string) {
+  if string.toLowerCase() === string {
   return 'I cant\'t hear you!' }
-  if (string.toUpperCase() === string) {
+  if string.toUpperCase() === string {
   return 'YES INDEED!' }
-  if (string'I love you, Grandma.') {
+  if 'I love you, Grandma.' {
   return 'I love you, too.'}
 }
 
